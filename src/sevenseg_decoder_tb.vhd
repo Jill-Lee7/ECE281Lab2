2aca5fa0cf81ccbd4440f559892ccadaf595bb54
Jill-Lee7 <Jill-Lee7@users.noreply.github.com>
2025-02-25 20:37:37 -0700

    I'm gonna cry if this doesn't work

--- src/sevenseg_decoder_tb.vhd
+++ src/sevenseg_decoder_tb.vhd
@@ -1,14 +1,4 @@
 --+----------------------------------------------------------------------------
---| 
---| COPYRIGHT 2025 United States Air Force Academy All rights reserved.
---| 
---| United States Air Force Academy     __  _______ ___    _________
---| Dept of Electrical &               / / / / ___//   |  / ____/   |
---| Computer Engineering              / / / /\__ \/ /| | / /_  / /| |
---| 2354 Fairchild Drive Ste 2F6     / /_/ /___/ / ___ |/ __/ / ___ |
---| USAF Academy, CO 80840           \____//____/_/  |_/_/   /_/  |_|
---| 
---| ---------------------------------------------------------------------------
 --|
 --| FILENAME      : sevenseg_decoder_tb.vhd
 --| AUTHOR(S)     : Jillian Essig
@@ -17,35 +7,8 @@
 --|                works properly for all 16 hex values (0-F).
 --|
 --+----------------------------------------------------------------------------
---|
---| REQUIRED FILES :
---|
---|    Libraries : ieee
---|    Packages  : std_logic_1164, numeric_std
---|    Files     : sevenseg_decoder.vhd
---|
---+----------------------------------------------------------------------------
---|
---| NAMING CONVENTIONS :
---|
---|    xb_<port name>           = off-chip bidirectional port ( _pads file )
---|    xi_<port name>           = off-chip input port         ( _pads file )
---|    xo_<port name>           = off-chip output port        ( _pads file )
---|    b_<port name>            = on-chip bidirectional port
---|    i_<port name>            = on-chip input port
---|    o_<port name>            = on-chip output port
---|    c_<signal name>          = combinatorial signal
---|    f_<signal name>          = synchronous signal
---|    ff_<signal name>         = pipeline stage (ff_, fff_, etc.)
---|    <signal name>_n          = active low signal
---|    w_<signal name>          = top level wiring signal
---|    g_<generic name>         = generic
---|    k_<constant name>        = constant
---|    v_<variable name>        = variable
---|    sm_<state machine type>  = state machine type definition
---|    s_<signal name>          = state name
---|
---+----------------------------------------------------------------------------
+
+
 
 library ieee;
 use ieee.std_logic_1164.all;
